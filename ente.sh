767deeb8ed491062951c920ada6b312752c8066f
Mr-DOY <83167364+Mr-DOY@users.noreply.github.com>
2021-06-02 22:17:33 +0700

    Add files via upload

---
+++ ente.sh
@@ -0,0 +1,41 @@
+clear
+echo "                                    TOOLS : 404 GATEWAY :V "
+echo " _________________________________________________________"
+echo "             [+] AUTHOR : Mr.DOY / Mrs.ADOY [+] "
+echo "             [+] TEAM   : DOY X-PUNTEN      [+] "
+echo "             [+] INSTAGRAM : @doy_xpunten_  [+] "
+echo " _________________________________________________________"
+echo "             [+] JANGAN SOMBONG,            [+] "
+echo "             [+]        DIMATA TUHAN.       [+] "
+echo "             [+]               KITA SAMA AE [+] "
+echo " _________________________________________________________"
+echo "             [+] DAH LAH KELAMAAN INTRO.    [+] "
+echo "             [+]           MENDING LANJUT.  [+] "
+echo " _________________________________________________________"
+echo "                      [+] WARNING [+]    "
+echo "             sebelum mulai mending lu follow dah "
+echo "                        @doy_xpunten_ "
+echo " _________________________________________________________"
+echo "                  》》》》 SULAP 《《《《 "
+echo " _________________________________________________________"
+echo " SILAHKAN PILIH TOOLS DIBAWAH INI : "
+echo " 1.MENGHAPUS TEXS "
+echo " _________________________________________________________"
+echo "                      [+]   MAAF  [+] "
+echo "              TOOLS INI BARU DIBUAT SEMAKSIMAL "
+echo "                 JIKA MAU LEBIH ANDA BISA "
+echo "                   REQUEST TOOLS KEPADA "
+echo "                       @doy_xpunten_ "
+echo " _________________________________________________________"
+read -p " [+]  Silahkan masukkan pilhan anda : " pilih
+
+if $1 
+then 
+clear
+echo " [+] THANKS YOU  : YANG UDAH GUNAKAN SCRIPT GAK GUNA [+] "
+echo " [+] WE ARE FROM : DOY X-PUNTEN                      [+] "
+echo "___________________________________________________________ "
+echo "     [+] GITHUB PUNYA SEMUA LAH MASA PUNYA GW [+] "
+echo "___________________________________________________________"
+fi
+
